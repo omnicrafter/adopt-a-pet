7061e2f946641b3cdb65b3472b2b1e8d3cf0e8bb
Jeff Chen <jeffc@Soma-3.local>
2023-10-16 18:14:00 -0500

    added validation to add pet form

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, BooleanField, IntegerField, SelectField, TextAreaField
+from wtforms import StringField, FloatField, BooleanField, IntegerField, SelectField, TextAreaField, RadioField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, Email, AnyOf, NoneOf
 
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField("Pet Name")
-    species = SelectField("Species", choices=[
-                          ('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
-    photo_url = StringField("Photo URL")
-    age = IntegerField("Age")
-    notes = TextAreaField("Notes")
+    name = StringField("Pet Name", validators=[
+                       InputRequired(message="Pet name cannot be blank")])
+    species = RadioField("Species", choices=[
+        ('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired(), AnyOf(['cat', 'dog', 'porcupine'], message="Species must be cat, dog, or porcupine")])
+    photo_url = StringField("Photo URL", validators=[
+                            Optional(), URL(message="Please enter a valid URL")])
+    age = IntegerField("Age", validators=[
+                       Optional(), NumberRange(min=0, max=30, message="Age must be between 0 and 30")])
+    notes = TextAreaField("Notes", validators=[Optional()])
--- seed.py
+++ seed.py
@@ -1,23 +1,28 @@
 from models import Pet, db
 from app import app
 
-db.drop_all()
-db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-# Add pets
-pet1 = Pet(name='Pikachu', species='Mouse',
-           age=2, notes='Likes to shock people')
-pet2 = Pet(name='Charmander', species='Lizard', age=4, notes='Breathes fire')
-pet3 = Pet(name='Eevee', species='Fox', age=1, notes='Loves to play')
-pet4 = Pet(name='Meowth', species='Cat', age=3, notes='Team Rocket')
-pet5 = Pet(name='Psyduck', species='Duck', age=2, notes='Has a headache')
-pet6 = Pet(name='Squirtle', species='Turtle', age=5, notes='Loves water')
-pet7 = Pet(name='Bulbasaur', species='Dinosaur', age=6, notes='Likes to eat')
-pet8 = Pet(name='Jigglypuff', species='Balloon', age=1, notes='Loves to sing')
-pet9 = Pet(name='Snorlax', species='Bear', age=7, notes='Loves to sleep')
-pet10 = Pet(name='Charizard', species='Dragon', age=8, notes='Breathes fire')
+    # Add pets
+    pet1 = Pet(name='Pikachu', species='Mouse',
+               age=2, notes='Likes to shock people')
+    pet2 = Pet(name='Charmander', species='Lizard',
+               age=4, notes='Breathes fire')
+    pet3 = Pet(name='Eevee', species='Fox', age=1, notes='Loves to play')
+    pet4 = Pet(name='Meowth', species='Cat', age=3, notes='Team Rocket')
+    pet5 = Pet(name='Psyduck', species='Duck', age=2, notes='Has a headache')
+    pet6 = Pet(name='Squirtle', species='Turtle', age=5, notes='Loves water')
+    pet7 = Pet(name='Bulbasaur', species='Dinosaur',
+               age=6, notes='Likes to eat')
+    pet8 = Pet(name='Jigglypuff', species='Balloon',
+               age=1, notes='Loves to sing')
+    pet9 = Pet(name='Snorlax', species='Bear', age=7, notes='Loves to sleep')
+    pet10 = Pet(name='Charizard', species='Dragon',
+                age=8, notes='Breathes fire')
 
-db.session.add_all([pet1, pet2, pet3, pet4, pet5,
-                   pet6, pet7, pet8, pet9, pet10])
+    db.session.add_all([pet1, pet2, pet3, pet4, pet5,
+                        pet6, pet7, pet8, pet9, pet10])
 
-db.session.commit()
+    db.session.commit()
